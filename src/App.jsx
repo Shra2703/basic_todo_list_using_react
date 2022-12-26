3134942a142bc153fd6deb34b36e92c7759f3180
Shraddha Goyal <shraddhagoyal800@gmail.com>
2022-12-26 23:04:32 +0530

    add function is addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-const App = () =>{
-    return(
-        <>
-        <div className="main_div">
+const App = () => {
+  const [item, setItem] = useState(" ");
+  const [itemArr, setItemArr] = useState([]);
+
+  const itemEvent = (e) => {
+    setItem(e.target.value);
+  };
+
+  const itemAdded = () => {
+    setItemArr((oldValue) => {
+      return [...oldValue, item];
+    })
+  };
+  return (
+    <>
+      <div className="main_div">
         <div className="center_div">
-            <br />
-            <h1>ToDo List</h1>
-            <br />
-            <input type="text" placeholder='Add a Item' />
-            <button> + </button>
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a Item" onChange={itemEvent} />
+          <button onClick={itemAdded}> + </button>
 
-            <ol>
-                <li>buy apples</li>
-            </ol>
-        </div>
-        </div>
+          <ol>
+            {/* <li>{item}</li> */}
 
-        </>
-    );
+            {itemArr.map((itemValue) => {
+              return <li>{itemValue}</li>;
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 };
-
 
 export default App;
